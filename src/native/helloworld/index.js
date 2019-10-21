9f778477dc835e77b3dc3599fbfcc580d331df40
muukojima <muu.kojima.dev@gmail.com>
2019-10-21 23:39:54 +0900

    Add comment

--- src/native/helloworld/index.js
+++ src/native/helloworld/index.js
@@ -1,11 +1,34 @@
+
+// HTMLElement が html要素の最上位クラスになる
 class HelloWorld extends HTMLElement {
+  // 初期化処理、 イベントリスナー, ShadowDOMの作成
+  // カスタムエレメントのコンストラクタで行う処理(whatwg参考)
+  // @see: https://html.spec.whatwg.org/multipage/custom-elements.html#custom-element-conformance
   constructor() {
+    // HTMLElementのコンストラクタを呼ぶ
     super();
+    // Shadow DOMの作成
     const shadowRoot = this.attachShadow({mode: 'open'});
     shadowRoot.innerHTML = `
-      <h1 style="color:blue;">Hello! Web Components!</h1>
+      <style>
+        /* 親ページのルールによる指定の優先度が、要素で定義されている :host ルールよりも高いので、外部のスタイルが優先される */
+        :host {
+          /* カスタムエレメントはデフォルトでinlne要素 */
+          display: block;
+        }
+        /* h1要素に当ててスタイルがここ以外の境界線のの外に出ないかたしかめる */
+        h1 {
+          color: blue;
+          font-family: Helvetica, Arial, sans-serif;
+          text-align: center;
+          font-weigh: bold;
+          font-size: 50px;
+        }
+      </style>
+      <h1>Hello!</h1>
     `;
   }
 }
 
+// カスタムエレメントの登録
 window.customElements.define('x-helloworld', HelloWorld);
