51b99f28c605aea39c7cc015d5b08dc0c1f1cfd1
muukojima <muu.kojima.dev@gmail.com>
2019-10-22 18:02:24 +0900

    Add todo dir

--- src/native/todo/index.js
+++ src/native/todo/index.js
@@ -1,6 +1,24 @@
+const template = document.createElement('template');
+template.innerHTML = `
+  <style>
+  :host {
+    display: block;
+  }
+  h1 {
+    color: black;
+    font-family: Helvetica;
+    text-align: center;
+    font-size: 50px;
+  }
+  </style>
+  <h1>Todo App</h1>
+`;
+
 class App extends HTMLElement {
   constructor() {
     super();
+    const shadowRoot = this.attachShadow({mode: 'open'});
+    shadowRoot.appendChild(template.content.cloneNode(true));
   }
 }
 
