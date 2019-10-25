ccedccd6ee6b99ec93299103c609db7dee69806e
MuuKojima <muu.kojima.dev@gmail.com>
2019-10-26 01:00:23 +0900

    Make it possible to add todo

--- src/native/todo/index.js
+++ src/native/todo/index.js
@@ -3,10 +3,11 @@ template.innerHTML = `
   <style>
   :host {
     display: block;
+    font-family: Helvetica;
+    font-weight: bold;
   }
   h1 {
     color: black;
-    font-family: Helvetica;
     text-align: center;
     font-size: 50px;
   }
@@ -15,12 +16,17 @@ template.innerHTML = `
     align-items: flex-start;
     justify-content: center;
   }
+  li {
+    font-size: 24px;
+    color: gray;
+  }
   </style>
   <h1>Todo App</h1>
   <form>
     <input type="text"></input>
     <button type="button">submit</button>
   </form>
+  <ul></ul>
 `;
 
 class App extends HTMLElement {
@@ -28,16 +34,26 @@ class App extends HTMLElement {
     super();
     this._shadowRoot = this.attachShadow({mode: 'open'});
     this._shadowRoot.appendChild(template.content.cloneNode(true));
-    this._totoList = [];
+    this._todoList = [];
     this.submitBtn = this._shadowRoot.querySelector('button');
-    this.submitBtn.addEventListener('click', () => {
-      const input = this._shadowRoot.querySelector('input');
-      this._add(input.value);
+    this.submitBtn.addEventListener('click', () => this._add());
+    this.todoListElm = this._shadowRoot.querySelector('ul');
+  }
+
+  _render() {
+    this.todoListElm.innerHTML = '';
+    this._todoList.forEach(item => {
+      const todoElm = document.createElement('li');
+      todoElm.innerHTML = item.label;
+      this.todoListElm.appendChild(todoElm);
     });
   }
 
-  _add(val) {
-    console.log(val);
+  _add() {
+    const inputElm = this._shadowRoot.querySelector('input');
+    this._todoList.push({label: inputElm.value, isChecked: false});
+    inputElm.value = '';
+    this._render();
   }
 }
 
