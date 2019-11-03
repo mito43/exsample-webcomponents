1f3c510ad7ba261947f3f27fef35efb8f155c88a
muukojima <muu.kojima.dev@gmail.com>
2019-11-03 17:33:42 +0900

    Add owc-dev-server

---
+++ src/lit-html/app.js
@@ -0,0 +1,96 @@
+import {html, render} from 'lit-html';
+import './todo.js';
+
+const template = document.createElement('template');
+template.innerHTML = `
+  <style>
+  :host {
+    display: block;
+    font-family: Helvetica;
+    font-weight: bold;
+    color: black;
+  }
+  h1 {
+    text-align: center;
+    font-size: 50px;
+  }
+  form {
+    text-align: center;
+    margin-bottom: 20px;
+  }
+  #container x-todo {
+    text-align: center;
+  }
+  </style>
+  <h1>Todo App</h1>
+  <form>
+    <input type="text"></input>
+    <button type="button">submit</button>
+  </form>
+  <div id="container"></div>
+`;
+
+class App extends HTMLElement {
+  constructor() {
+    super();
+    this._shadowRoot = this.attachShadow({mode: 'open'});
+    this._shadowRoot.appendChild(template.content.cloneNode(true));
+    this._todoList = [
+      {
+        label: 'TaskA',
+        checked: false,
+      },
+      {
+        label: 'TaskB',
+        checked: true,
+      },
+      {
+        label: 'TaskC',
+        checked: false,
+      }
+    ];
+    this._container = this._shadowRoot.getElementById('container');
+    this.submitBtn = this._shadowRoot.querySelector('button');
+    this.submitBtn.addEventListener('click', () => this._add());
+  }
+
+  connectedCallback() {
+    this._render();
+  }
+
+  _render() {
+    this._container.innerHTML = '';
+    this._todoList.forEach((item, index) => {
+      const todoElm = document.createElement('x-todo');
+      todoElm.addEventListener('onToggle', this._toggle.bind(this));
+      todoElm.addEventListener('onRemove', this._remove.bind(this));
+      todoElm.label = item.label;
+      todoElm.checked = item.checked;
+      todoElm.index = index;
+      this._container.appendChild(todoElm);
+    });
+  }
+
+  _add() {
+    const inputElm = this._shadowRoot.querySelector('input');
+    this._todoList.push({label: inputElm.value, checked: false});
+    inputElm.value = '';
+    this._render();
+  }
+
+  _toggle(e) {
+    const todo = this._todoList[e.detail];
+    const hoge = Object.assign({}, todo, {
+      checked: !todo.checked
+    });
+    this._todoList[e.detail] = hoge;
+    this._render();
+  }
+
+  _remove(e) {
+    this._todoList.splice(e.detail, 1);
+    this._render();
+  }
+}
+
+window.customElements.define('x-app', App);
---
+++ src/lit-html/todo.js
@@ -0,0 +1,101 @@
+const template = document.createElement('template');
+template.innerHTML = `
+  <style>
+    :host {
+      display: block;
+      font-family: Helvetica;
+      font-weight: bold;
+      padding: 10px;
+    }
+  </style>
+  <input type="checkbox">
+  <label></label>
+  <button type="button">remove</button>
+`;
+
+class HTMLTodoElement extends HTMLElement {
+  static get observedAttributes() {
+    return ['label', 'checked', 'index'];
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    switch(name){
+      case 'label':
+        this._label = newValue;
+        break;
+      case 'checked':
+        this._checked = newValue;
+        break;
+      case 'index':
+        this._index = newValue;
+        break;
+    }
+    this._render();
+  }
+
+  constructor() {
+    super();
+    this._shadowRoot = this.attachShadow({ 'mode': 'open' });
+    this._shadowRoot.appendChild(template.content.cloneNode(true));
+    this._label =  '';
+    this._index = 0;
+    this._checked =  false;
+    this._checkBoxElm = this._shadowRoot.querySelector('input');
+    this._checkBoxElm.addEventListener('click', () => {
+      this.dispatchEvent(new CustomEvent('onToggle', { detail: this._index }));
+    });
+    this._removeElm = this._shadowRoot.querySelector('button');
+    this._removeElm.addEventListener('click', () => {
+      this.dispatchEvent(new CustomEvent('onRemove', { detail: this._index }));
+    });
+    this._labelElm = this._shadowRoot.querySelector('label');
+  }
+
+  connectedCallback() {
+    this._render();
+  }
+
+  _render() {
+    this._labelElm.innerHTML = this._label;
+    this._checkBoxElm.checked = this._checked;
+    this._labelElm.style.textDecoration = this._checked ? 'line-through'  : 'none';
+  }
+
+  get label() {
+    return this.getAttribute('label');
+  }
+
+  set label(val) {
+    if (val) {
+      this.setAttribute('label', val);
+    } else {
+      this.removeAttribute('label');
+    }
+  }
+
+  get checked() {
+    return this.getAttribute('checked');
+  }
+
+  set checked(val) {
+    if (val) {
+      this.setAttribute('checked', val);
+    } else {
+      this.removeAttribute('checked');
+    }
+  }
+
+  get index() {
+    return this.getAttribute('index');
+  }
+
+  set index(val) {
+    if (val) {
+      this.setAttribute('index', val);
+    } else {
+      this.removeAttribute('index');
+    }
+  }
+}
+
+window.customElements.define('x-todo', HTMLTodoElement);
