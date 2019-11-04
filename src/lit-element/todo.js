70ee42f87b524da0cee0e594a58ddb4e3084d49b
muukojima <muu.kojima.dev@gmail.com>
2019-11-04 21:04:17 +0900

    Add lit-element for todo

--- src/lit-element/todo.js
+++ src/lit-element/todo.js
@@ -1,101 +1,60 @@
 
-import { html, render } from 'lit-html';
+import { LitElement, html, css } from 'lit-element';
 
-class HTMLTodoElement extends HTMLElement {
-  static get observedAttributes() {
-    return ['label', 'checked', 'index'];
-  }
-
-  attributeChangedCallback(name, oldValue, newValue) {
-    switch(name){
-      case 'label':
-        this._label = newValue;
-        break;
-      case 'checked':
-        this._checked = this.hasAttribute('checked');
-        break;
-      case 'index':
-        this._index = Number(newValue);
-        break;
+class HTMLTodoElement extends LitElement {
+  static get properties() {
+    return {
+      label: {
+        type: String,
+        reflect: true
+      },
+      checked: {
+        type: Boolean,
+        reflect: true
+      },
+      index: {
+        type: Number
+      }
     }
-    render(this._template(), this._shadowRoot, {eventContext: this});
   }
 
   constructor() {
     super();
-    this._shadowRoot = this.attachShadow({ 'mode': 'open' });
-    this._label =  '';
-    this._index = 0;
-    this._checked =  false;
+    this.label =  '';
+    this.index = 0;
+    this.checked =  false;
   }
 
-  connectedCallback() {
-    render(this._template(), this._shadowRoot, {eventContext: this});
+  static get styles() {
+    return css`
+      :host {
+        display: block;
+        font-family: Helvetica;
+        font-weight: bold;
+        padding: 10px;
+      }
+      .completed {
+        text-decoration: line-through;
+      }
+    `;
   }
 
-  _template() {
+  render() {
     return html`
-      <style>
-        :host {
-          display: block;
-          font-family: Helvetica;
-          font-weight: bold;
-          padding: 10px;
-        }
-        .completed {
-          text-decoration: line-through;
-        }
-      </style>
       <input type="checkbox"
-        .checked=${this._checked}
+        .checked=${this.checked}
         @change=${this._dispatchToggle}>
-      <label class=${this._checked ? 'completed' : ''}>${this._label}</label>
+      <label class=${this.checked ? 'completed' : ''}>${this.label}</label>
       <button type="button" @click=${this._dispatchRemove}>remove</button>
     `;
   }
 
   _dispatchToggle() {
-    this.dispatchEvent(new CustomEvent('onToggle', { detail: this._index }));
+    this.dispatchEvent(new CustomEvent('onToggle', { detail: this.index }));
   }
 
   _dispatchRemove() {
-    this.dispatchEvent(new CustomEvent('onRemove', { detail: this._index }));
-  }
-
-  get label() {
-    return this.getAttribute('label');
-  }
-
-  set label(val) {
-    if (val) {
-      this.setAttribute('label', val);
-    } else {
-      this.removeAttribute('label');
-    }
-  }
-
-  get checked() {
-    return this.getAttribute('checked');
-  }
-
-  set checked(val) {
-    if (val) {
-      this.setAttribute('checked', '');
-    } else {
-      this.removeAttribute('checked');
-    }
-  }
-
-  get index() {
-    return this.getAttribute('index');
-  }
-
-  set index(val) {
-    if (val) {
-      this.setAttribute('index', val);
-    } else {
-      this.removeAttribute('index');
-    }
+    this.dispatchEvent(new CustomEvent('onRemove', { detail: this.index }));
   }
 }
 
